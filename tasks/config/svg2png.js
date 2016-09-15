14d10c27cec2e8a40be93e81088fc6a884601fa3
Chaz Chumley <chaz.chumley@gmail.com>
2016-09-15 17:10:53 -0400

    update project with webstarter

---
+++ tasks/config/svg2png.js
@@ -0,0 +1,18 @@
+// https://github.com/dbushell/grunt-svg2png
+
+module.exports = function(grunt) {
+
+  grunt.config.set('svg2png', {
+    dist: {
+      files: [{
+        expand: false,
+        flatten: true,
+        cwd: '<%= pkg.themePath %>/images/bg/',
+        src: ['*.svg'],
+        dest: '<%= pkg.themePath %>/images/bg/'
+      }]
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-svg2png');
+};
