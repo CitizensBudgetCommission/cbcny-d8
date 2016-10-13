e624c572e6ed64c7816f7ba15bb522fef5fb03cf
Russell Stoll <rrstoll@forumone.com>
2016-10-13 17:08:01 -0400

    More work

--- web/themes/cbcny_theme/js/scripts.js
+++ web/themes/cbcny_theme/js/scripts.js
@@ -21,13 +21,13 @@
     TO = setTimeout(resizeStuff, 200);
   }).resize();
 
-  // add .fixed class to #header 
+  // add .header-is-fixed class to .page 
   $(window).scroll(function() {
     var value = $(this).scrollTop();
-    if (value > 93)
-      $("#header").addClass("is-fixed");
+    if (value > 148)
+      $(".page").addClass("header-is-fixed");
     else
-      $("#header").removeClass("is-fixed");
+      $(".page").removeClass("header-is-fixed");
   });
 
 })(jQuery);
