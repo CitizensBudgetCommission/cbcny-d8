8af9d8cd556be02c7b24a22a6cf13dfe30db83b8
Campbell Vertesi <campbell@vertesi.com>
2016-11-07 01:09:29 +0100

    Skeleton migrate_cbc module with node text source plugin

---
+++ web/modules/custom/migrate_cbc/src/Plugin/migrate/source/CbcParagraphText.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\migrate_cbc\plugin\migrate\source;
+
+/**
+ * Source plugin for full_text paragraphs. Gets the node body out of particular
+ * types, and turns it into paragraphs.
+ *
+ * @MigrateSource(
+ *   id = "cbc_paragraph_text"
+ * )
+ */
+class CbcParagraphText extends D6Node {
+
+/**
+ * Class CbcParagraphText
+ * @package Drupal\migrate_cbc\plugin\migrate\source
+ */
+class CbcParagraphText extends D6Node {
+
+}
