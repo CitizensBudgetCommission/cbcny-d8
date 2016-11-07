13c19caf5ac14ba6d7a5fabb2b5ea5b58a85ac85
Campbell Vertesi <campbell@vertesi.com>
2016-11-07 09:41:41 +0100

    Add blog node migration, fill it out along with node_paragraph

--- web/modules/custom/migrate_cbc/src/Plugin/migrate/source/CbcParagraphText.php
+++ web/modules/custom/migrate_cbc/src/Plugin/migrate/source/CbcParagraphText.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\migrate_cbc\plugin\migrate\source;
 
+use Drupal\node\Plugin\migrate\source\d6\Node;
+
 /**
  * Source plugin for full_text paragraphs. Gets the node body out of particular
  * types, and turns it into paragraphs.
@@ -10,12 +12,16 @@ namespace Drupal\migrate_cbc\plugin\migrate\source;
  *   id = "cbc_paragraph_text"
  * )
  */
-class CbcParagraphText extends D6Node {
+class CbcParagraphText extends Node {
 
-/**
- * Class CbcParagraphText
- * @package Drupal\migrate_cbc\plugin\migrate\source
- */
-class CbcParagraphText extends D6Node {
-
+  public function query() {
+    $query = parent::query();
+    // Only migrate published content
+    $query->condition('n.status', 1);
+    // Offer a list of source content types.
+    if (isset($this->configuration['node_type_list'])) {
+      $query->condition('n.type', $this->configuration['node_type_list'], 'IN');
+    }
+    return $query;
+  }
 }
