a923e8d0a90731ff3c5c39fa2d0d4a6c99a66728
Campbell Vertesi <campbell@vertesi.com>
2016-11-08 17:55:29 +0100

    Add fids to the node migration source

--- web/modules/custom/migrate_cbc/src/Plugin/migrate/source/CbcParagraphText.php
+++ web/modules/custom/migrate_cbc/src/Plugin/migrate/source/CbcParagraphText.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\migrate_cbc\plugin\migrate\source;
 
+use Drupal\migrate\Row;
 use Drupal\node\Plugin\migrate\source\d6\Node;
 
 /**
@@ -23,5 +24,38 @@ class CbcParagraphText extends Node {
       $query->condition('n.type', $this->configuration['node_type_list'], 'IN');
     }
     return $query;
+  }
+
+  public function fields() {
+    return parent::fields() + [
+      'files' => $this->t('Files attached directly to the D6 node'),
+    ];
+  }
+
+  public function prepareRow(Row $row) {
+    $row = parent::prepareRow($row);
+    $row = $this->addFiles($row);
+    return $row;
+  }
+
+  /**
+   * Add files to the row.
+   * OK, this could just as easily have happened in $this->query(). But technically
+   * joins are SUPPOSED to happen in prepareRow so as not to slow up the primary
+   * query when it's used for counts, etc. Plus, this makes a better example.
+   *
+   * @param \Drupal\migrate\Row $row
+   * @return \Drupal\migrate\Row
+   */
+  private function addFiles(Row $row) {
+    $query = $this->select('upload', 'u')
+      ->distinct()
+      ->fields('u', array('nid', 'vid'));
+    $query->condition('u.nid', $row->getSourceProperty('nid'));
+    $fids = $query->execute()
+      ->fetchCol(0);
+    $row->setSourceProperty('files', $fids);
+
+    return $row;
   }
 }
