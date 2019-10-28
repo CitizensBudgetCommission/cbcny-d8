18a646aae53c2c935e282d66fdf0f5cdf2069637
Gergely Csonka <gergely.csonka@gmail.com>
2019-10-28 00:59:07 -0400

    Add commerce modules for the CBC Dinner functionality.

---
+++ drupal8/web/modules/custom/cbc_dinner/src/Controller/CBCController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\cbc_dinner\Controller;
+
+/**
+ * Class CBCController
+ * @package Drupal\cbc_dinner\Controller
+ */
+
+Class CBCController {
+
+  public function itemlisting() {
+    return [];
+  }
+}
