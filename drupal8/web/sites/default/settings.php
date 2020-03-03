b2f12260400078473240016d4c3461629707f2f9
Péter Serfőző <peter.serfozo91@gmail.com>
2020-03-03 16:43:44 +0100

    Update settings.php files and remaining modules.

--- drupal8/web/sites/default/settings.php
+++ drupal8/web/sites/default/settings.php
@@ -53,13 +53,7 @@ $config['image.settings']['allow_insecure_derivatives'] = TRUE;
 $settings['file_private_path'] = 'sites/default/files/private';
 
 // Set up a config sync directory.
-//
-// This is defined inside the read-only "config" directory. This works well,
-// however it requires a patch from issue https://www.drupal.org/node/2607352
-// to fix the requirements check and the installer.
-$config_directories = [
-  CONFIG_SYNC_DIRECTORY => '../config/sync',
-];
+$settings['config_sync_directory'] = '../config/sync';
 
 $settings['update_free_access'] = FALSE;
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
