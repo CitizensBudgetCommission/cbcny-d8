1f64f79c7c8b884388265af87ada762b1c77a2a1
Denes Gulyas <denes.gulyas@cheppers.com>
2020-06-04 16:01:04 +0200

    Issue #30125 - Fix stuck menu css and jquery once error

--- drupal8/web/themes/cbcny_theme/js/dropdown.js
+++ drupal8/web/themes/cbcny_theme/js/dropdown.js
@@ -4,14 +4,14 @@
 
   Drupal.behaviors.menuDropdown = {
     attach: function (context) {
-
       $('.js-has-subnav', context).once('menuDropdown').each(function () {
         $(this).hover(function () {
           $(this).children('.js-menu-dropdown').addClass('is-open-menu').wrap('<div class="js-menu-dropdown-wrapper"></div>');
         },
-          function () {
-            $(this).children('.js-menu-dropdown-wrapper').children('.js-menu-dropdown').unwrap().removeClass('is-open-menu');
-          })
+
+        function () {
+          $(this).children('.js-menu-dropdown-wrapper').children('.js-menu-dropdown').unwrap().removeClass('is-open-menu');
+        })
       });
     }
   };
