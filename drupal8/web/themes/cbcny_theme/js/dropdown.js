a2755b721476b37479b74eb1b43d5db169c75ae4
Denes Gulyas <denes.gulyas@cheppers.com>
2020-06-04 10:30:44 +0200

    Issue #30125 - Fix desktop dropdown menu

--- drupal8/web/themes/cbcny_theme/js/dropdown.js
+++ drupal8/web/themes/cbcny_theme/js/dropdown.js
@@ -7,10 +7,10 @@
 
       $('.js-has-subnav', context).once('menuDropdown').each(function () {
         $(this).hover(function () {
-          $(this).children('.js-menu-dropdown').addClass('is-open-menu');
+          $(this).children('.js-menu-dropdown').addClass('is-open-menu').wrap('<div class="js-menu-dropdown-wrapper"></div>');
         },
           function () {
-            $(this).children('.js-menu-dropdown').removeClass('is-open-menu');
+            $(this).children('.js-menu-dropdown-wrapper').children('.js-menu-dropdown').unwrap().removeClass('is-open-menu');
           })
       });
     }
